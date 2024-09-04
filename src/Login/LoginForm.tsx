6985e4bf176a16dc649877119714dcd15b263ace
Kribbz0r <kribbz0r@gmail.com>
2024-09-04 22:25:40 +0200

    CreatePlayer and Login fixed

---
+++ src/Login/LoginForm.tsx
@@ -0,0 +1,50 @@
+import { FormEvent, useState } from "react";
+
+interface LoginFormProps {
+    onLogin: (loggedInPlayer: string) => void;
+}
+
+function LoginForm({ onLogin }: LoginFormProps) {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>, username: string, password: string): void {
+        e.preventDefault();
+
+        fetch("http://localhost:8080/player/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Origin": "*"
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                // Nödlösning. Fixa i backend
+                if (data !== "Wrong password" || "Something went wrong") {
+                    const loggedInPlayer = data;
+                    localStorage.setItem("loggedInPlayer", loggedInPlayer)
+                    onLogin(loggedInPlayer);
+                    setUsername("");
+                    setPassword("");
+                } else {
+                    alert("Wrong username or password")
+                }
+            })
+    }
+    return (
+        <>
+            <form className="loginForm" onSubmit={(e) => handleSubmit(e, username, password)}>
+                <input type="text" value={username} onChange={((e) => setUsername(e.target.value))}></input>
+                <input type="password" value={password} onChange={((e) => setPassword(e.target.value))}></input>
+                <button type="submit">Logga in</button>
+            </form>
+        </>
+    )
+}
+export default LoginForm;
